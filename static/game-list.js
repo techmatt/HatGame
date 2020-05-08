a01ecdb8100bd935e70f6c41e12c36c2b6ee780c
Peter Lofgren <plofgren@cs.stanford.edu>
2020-05-07 21:11:19 -0700

    Format game-list.js

--- static/game-list.js
+++ static/game-list.js
@@ -4,31 +4,31 @@
 const e = React.createElement;
 
 class GameList extends React.Component {
-    constructor(props) {
-	super(props);
-	this.state = { gameList: ["loading"]};
-	this.requestGameList();  
-    }
+	constructor(props) {
+		super(props);
+		this.state = { gameList: ["loading"] };
+		this.requestGameList();
+	}
 
-    requestGameList() {
-	fetch('api/gamelist')
-	.then(response => response.json())
-	    .then(data => {
-		 console.log("got data %o", data);
-		  const games = data["games"];
-		console.log("got gamelist %o", games);
-		this.setState({gameList: games});
-	    });
-    }
-    
-    render() {
-	return e(
-	    'ul',
-	    null,
-	    this.state.gameList.map(gameName =>
-				    e('li', null, gameName))
-	    );
-    }
+	requestGameList() {
+		fetch('api/gamelist')
+			.then(response => response.json())
+			.then(data => {
+				console.log("got data %o", data);
+				const games = data["games"];
+				console.log("got gamelist %o", games);
+				this.setState({ gameList: games });
+			});
+	}
+
+	render() {
+		return e(
+			'ul',
+			null,
+			this.state.gameList.map(gameName =>
+				e('li', null, gameName))
+		);
+	}
 }
 
 const domContainer = document.querySelector('#game_list_container');
